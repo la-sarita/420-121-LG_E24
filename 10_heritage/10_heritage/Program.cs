3696f8b107adbdeda8f6a2a15b94e62b101f4a87
la-sarita <sara.boumehraz@collegemv.qc.ca>
2024-07-03 16:06:10 -0400

    Ajout du code de l'heritage

---
+++ 10_heritage/10_heritage/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_heritage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicule vehicule = new Vehicule("BMW", 340, 30);
+            Voiture voiture1 = new Voiture("Honda", 200, 100, 4);
+            Moto moto = new Moto("Yamaha", 300, 40, 12);
+            Console.WriteLine(vehicule);
+            Console.WriteLine(voiture1);
+            Console.WriteLine(moto);
+            moto.SetVitesse(150);
+            voiture1.SetVitesse(380);
+            Console.WriteLine(voiture1);
+            Console.WriteLine(moto);
+        }
+    }
+}
